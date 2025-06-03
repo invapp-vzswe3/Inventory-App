425063327bd772505d4c249bc097213a91fbff37
bpatel2001 <bpatel2001h@gmail.com>
2025-06-03 14:21:06 -0500

    Finished single item view and added functionality to switch views on click

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -5,25 +5,43 @@ import apiURL from "../api";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [singleItem, setSingleItem] = useState(null);
 
   useEffect(() => {
     const fetchItems = async () => {
-    const response = await fetch(`${apiURL}/items`); // Fetch items from the API
-    const data = await response.json();
-    console.log(items)
-    setItems(data);
-  };
+      const response = await fetch(`${apiURL}/items`); // Fetch items from the API
+      const data = await response.json();
+      console.log(items)
+      setItems(data);
+    };
   fetchItems();// Fetch the items
   }, []);
+
+
+  const singleItemView = async (id) => {
+    const response = await fetch(`${apiURL}/items/${id}`); // Fetch a single item by ID
+    const data = await response.json();
+    setSingleItem(data);
+  }
 
   return (
     <>
       <h1>Inventory App</h1>
-     <ul>
-      {items.map(item => (
-        <li key={item.id}>{item.name} ${item.price} {item.description} {item.category} {item.image} </li>
-      ))}
-    </ul>
+      {!singleItem ? (
+        items.map(item => (
+          <button key={item.id} onClick={() => singleItemView(item.id)}>{item.name} ${item.price} {item.description} {item.category} {item.image} </button>
+        ))
+      ) : (
+        <div>
+          <h2>{singleItem.name}</h2>
+          <p>Price: ${singleItem.price}</p>
+          <p>Description: {singleItem.description}</p>
+          <p>Category: {singleItem.category}</p>
+          <img src={singleItem.image} alt={singleItem.name} />
+          <br />
+          <button onClick={() => setSingleItem(null)}>Back to Items</button>
+        </div>
+      )}
   </>
 );
 }
