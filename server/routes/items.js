f33616e463719d6dbfc2f20f97e0f7a9526afacf
bpatel2001 <bpatel2001h@gmail.com>
2025-06-03 13:14:18 -0500

    Created get route for individual users

--- server/routes/items.js
+++ server/routes/items.js
@@ -5,5 +5,16 @@ const router = express.Router();
 router.use(express.json());
 
 // Define your routes here
+router.get("/:id", async (req, res) => {
+    try{
+        const item = await Item.findByPk(req.params.id);
+        if (!item) {
+            return res.status(404).json({ error: "Item not found" });
+        }
+        res.json(item);
+    } catch (error) {
+        res.status(500).json({ error: "Server Error" });
+    }
+})
 
 module.exports = router;
