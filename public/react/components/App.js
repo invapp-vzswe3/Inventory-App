10f72b924a726f66e4055b2359e979bfffb0b07b
Joshua Benavides <joshua.benavides@verizon.com>
2025-06-04 14:33:33 -0500

    pulled from main and fixed conflict ONLY on my branch Add item still works and refreshes

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -42,14 +42,43 @@ function App() {
   return (
     <>
       <h1 className = "title">Inventory App</h1>
-      {!singleItem ? (
-        items.map(item => (
+      {isAddingProduct ? (
+      <form onSubmit={async (event) => {
+        event.preventDefault();
+        const productData = {
+          name: event.target.name.value,
+          description: event.target.description.value,
+          price: event.target.price.value,
+          category: event.target.category.value,
+          image: event.target.image.value,
+        };
+        await fetch(`${apiURL}/items`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(productData),
+        });
+        setIsAddingProduct(false);
+        const response = await fetch(`${apiURL}/items`);
+        setItems(await response.json());
+      }}>
+        <input name="name" placeholder="Name"/> <br />
+        <input name="description" placeholder="Description" /> <br />
+        <input name="price" placeholder="Price"/> <br />
+        <input name="category" placeholder="Category" /> <br />
+        <input name="image" placeholder="Image URL" /> <br />
+        <button type="submit">Add Item</button>
+        <button type="button" onClick={() => setIsAddingProduct(false)}>Cancel</button>
+      </form> ) : !singleItem ? (
+        <>
+        {items.map(item => (
           <div onClick={() => singleItemView(item.id)} className = "items" key = {item.id}>
             <h2>{item.name}</h2>
             <img className = "itemimage" src = {item.image} alt={item.name} width="20%" height="20%"/> 
             <h3 className = "price">Price: ${item.price}</h3>
           </div>
-        ))
+        ))}
+        <button onClick={(handleCreationClick)}>Add Item</button>
+        </>
       ) : (
         <div>
           <h2>{singleItem.name}</h2>
