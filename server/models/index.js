3939d7d1491a2e3fa5ffc275a143f59350ff6eda
Emmanuel Luis <ejluis2001@gmail.com>
2025-06-06 15:15:28 -0500

    Moved associations into index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,15 @@ const Item = require("./Item");
 const User = require("./User");
 const Order = require("./Order");
 const Cart = require("./Cart");
+
+Order.belongsTo(User);
+User.hasMany(Order);
+
+Cart.belongsTo(User);
+Cart.belongsTo(Item);
+User.hasMany(Cart);
+Item.hasMany(Cart);
+
 module.exports = {
   Item,
   User,
