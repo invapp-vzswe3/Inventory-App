fa0a6713eff64f039230e68b43cb4adc13fb2447
EmmanuelMultiverse <ejluis2001+verizon@gmail.com>
2025-06-06 17:36:45 -0500

    Fixing merge issues & setup testing configs for backend and frontend

---
+++ jest.client.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  displayName: 'Client Tests',
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/public/**/*.test.js'],
+};
---
+++ jest.server.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  displayName: 'Server Tests',
+  testEnvironment: 'node', 
+  testMatch: ['<rootDir>/server/__tests__/**/*.test.js'], 
+  forceExit: true 
+  
+};
